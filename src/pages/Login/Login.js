b56d6d9392b9c75b8ec550cc736815416c389d27
chaerlo127 <monynony0203@gmail.com>
2023-05-11 19:03:19 +0900

    #2 feat: login 화면 생성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Login.css'
 
 function Login() {
     return (
-        <div>
-            로그인 페이지
+        <div className="loginPage">
+            <h2>냉집사 관리자 페이지</h2>
+            <div className='loginForm'>
+            {/* todo: url 생성시 연결 진행 */}
+            <form method="post" action="url" id="login-form">
+            <input type="text" id = "inputId" placeholder={"이메일"} autoFocus/>
+            <input type="text" id = "inputPw" placeholder={"비밀번호"}/>
+            <input type='submit'/>
+            </form>
+            </div>
         </div>
     );
 }
