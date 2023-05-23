6eff0205b6e9fc4c5c5c89eae36beed6d8b2e771
sojungpp <sophie2458@naver.com>
2023-05-24 00:31:22 +0900

    #3 fix: 탈퇴 회원 로직 해당 페이지로 이동

--- src/pages/UserManage/WithdrawUserManage.js
+++ src/pages/UserManage/WithdrawUserManage.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import './UserManage.css'
+import React, { useEffect, useState, useRef } from 'react';
+import './WithdrawUserManage.css'
+import axios from 'axios';
+import Tr from './UserTr';
+import Paging from 'components/Paging.js'
 
-function UserManage() {
+const WithdrawUserManage = () => {
+    const [info, setInfo] = useState([]);
+
+    const nextId = useRef(11);
+
+    // 데이터 호출
+    useEffect(() => {
+        // axios.get('https://za8hqdiis4.execute-api.ap-northeast-2.amazonaws.com/dev/dev-ice-bulter-main/admin/users?active=true')
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => setInfo(res.data))
+        .catch(err => console.log(err));
+    }, []);
+
+    const handleRemove = (id) => {
+        console.log("성공");
+        // TODO
+    }
+
+    const handleEdit = (id) => {
+        console.log("성공2");
+        // TODO
+    }
+
     return (
         <div className='page'>
-            <div className='withdrawUserManageContainer'>
-                <p className="title">탈퇴 회원</p>
-                <div className="contentbar" />
-                <div className="content">
-                </div>
+        <div className='withdrawUserManageContainer'>
+            <div className='title'>탈퇴 회원</div>
+            <div className ='content'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>닉네임</th>
+                        <th>이메일</th>
+                        <th>신고 누적 횟수</th>
+                        <th>이메일 전송</th>
+                    </tr>
+                </thead>
+                <Tr info={info} handleRemove={handleRemove} handleEdit={handleEdit}/>
+            </table>
+            <div className='paging'><Paging/></div>
             </div>
         </div>
+        </div>
     );
-}
+};
 
-export default UserManage;
+export default WithdrawUserManage;
