f2bf0261e29065475da57482b9bff0571393c025
sojungpp <sophie2458@naver.com>
2023-05-27 20:34:10 +0900

    #3 프록시 서버 분리

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,6 +6,12 @@ module.exports = function (app) {
         createProxyMiddleware({
             target: 'https://za8hqdiis4.execute-api.ap-northeast-2.amazonaws.com/dev/dev-ice-bulter-main',
             changeOrigin: true,
+        }))
+    app.use(
+        '/users',
+        createProxyMiddleware({
+            target: 'https://za8hqdiis4.execute-api.ap-northeast-2.amazonaws.com/dev/dev-ice-bulter-recipe/admin',
+            changeOrigin: true,
         })
-    );
+        );
 };
