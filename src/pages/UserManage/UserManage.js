a13ebe462804f38cce9da9657bfa0bcc4119dbb5
sojungpp <sophie2458@naver.com>
2023-05-29 17:26:46 +0900

    #23 fix: 회원 탈퇴 처리 API 연결 완료

--- src/pages/UserManage/UserManage.js
+++ src/pages/UserManage/UserManage.js
@@ -27,7 +27,7 @@ const UserManage = () => {
 
   // TODO 서버 수정되면 수정
   const withdrawUser = (idx) => {
-    axios.delete(`admin/users/${idx}`)
+    axios.delete(`/admin/users/${idx}`)
     .then(res => {
         console.log('HTTP POST 요청 성공');
         // 성공적으로 요청을 보냈을 때 실행할 작업
