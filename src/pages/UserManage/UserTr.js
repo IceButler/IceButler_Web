576745d69cfecebb808813078d061e2fa9dd5897
sojungpp <sophie2458@naver.com>
2023-05-31 14:40:02 +0900

    #23 feat: 일반 회원 검색 조회 추가

--- src/pages/UserManage/UserTr.js
+++ src/pages/UserManage/UserTr.js
@@ -7,6 +7,9 @@ const Tr = ({ info, onSendEmail, withdrawUser }) => {
 
   const tdStyle = {
     position: 'relative',
+    flex: '1',
+    flexBasis: '0',
+    minWidth: '0',
   };
 
   const imgStyle = {
@@ -18,17 +21,21 @@ const Tr = ({ info, onSendEmail, withdrawUser }) => {
   return (
     <>
       {
-        info.map(item => {
-          return (
+        info.length > 0 ? (
+          info.map(item => (
             <tr key={item.userIdx}>
-              <td style={tdStyle} width="30%">{item.nickname}</td>
-              <td style={tdStyle} width="30%">{item.email}</td>
-              <td style={tdStyle} width="20%">{item.reportCount}</td>
-              <td style={tdStyle} width="10%" onClick={() => onSendEmail(item)}><img src={email} style={imgStyle} /></td>
-              <td style={tdStyle} width="10%" onClick={() => withdrawUser(item.userIdx)}><img src={withdraw} style={imgStyle} /></td>
+              <td style={{ ...tdStyle, flex: '2' }}>{item.nickname}</td>
+              <td style={{ ...tdStyle, flex: '3' }}>{item.email}</td>
+              <td style={{ ...tdStyle, flex: '1' }}>{item.reportCount}</td>
+              <td style={tdStyle} onClick={() => onSendEmail(item)}><img src={email} style={imgStyle} /></td>
+              <td style={tdStyle} onClick={() => withdrawUser(item.userIdx)}><img src={withdraw} style={imgStyle} /></td>
             </tr>
-          )
-        })
+          ))
+        ) : (
+          <tr>
+            <td colSpan={5} style={tdStyle}>검색 결과가 없습니다.</td>
+          </tr>
+        )
       }
     </>
   )
