fba05cfaf9e6f0a38179747012d80b828a5d1036
sojungpp <sophie2458@naver.com>
2023-06-02 02:07:39 +0900

    #34 feat: 일반 회원 이메일 전송 구현

--- src/pages/Email/Email.js
+++ src/pages/Email/Email.js
@@ -1,26 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import sendEmail from "assets/images/send.png";
+import React, { useRef, useState, useEffect } from 'react';
+import sendEmailUrl from "assets/images/send.png";
 import './Email.css'
+import emailjs from '@emailjs/browser';
+import { useLocation } from 'react-router-dom';
 
 
 const Email = () => {
+  const location = useLocation();
+  const email = location.state.item.email;
+  const nickname = location.state.item.nickname;
+
+  const form = useRef(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    var templateParams = {	
+            email : email,
+            title : e.target.elements.title.value,
+            message : e.target.elements.message.value
+               };
+
+    emailjs.send("service_d0rcavp","template_ypn2nr6", templateParams, 'UMuQ-efYiV6XCJ6_R')
+      .then((result) => {
+          console.log(result.text);
+          alert("전송되었습니다.");
+      }, (error) => {
+        alert("실패했습니다.");
+          console.log(error.text);
+      });
+  };
   
   return (
     <div className='page'>
       <div className='emailManageContainer'>
         <div className='userManageTitle'>이메일 전송</div>
+        <form ref={form} onSubmit={sendEmail}>
         <div className='emailManageContent'>
           <div className='userManageBar' />
           <div className='emailTitleBar'>
                 <label className="emailTitleName">제목</label>
-                <input type="text" className="emailTitle" placeholder='제목을 입력해주세요'/>
+                <input type="text" className="emailTitle" name="title" placeholder='제목을 입력해주세요' value={`[냉집사] ${nickname}님, 안내 메일 드립니다.`}></input>
           </div>
-            <textarea className="emailContent" rows="20" cols="50"></textarea>
-            <button className="sendEmailBtn" onClick="sendEmail()">
-            <img className='sendEmailIcon' src={sendEmail} alt="버튼 아이콘" />
+            <textarea className="emailContent" name="message" rows="20" cols="50">
+            {`${nickname}님, 안녕하세요.
+냉장고 유통기한 관리 어플, 냉집사입니다.`}</textarea>
+            <button className="sendEmailBtn" type='submit' value="Send">
+            <img className='sendEmailIcon' src={sendEmailUrl} alt="버튼 아이콘" />
                 보내기
             </button>
         </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/UserManage/UserTr.js
+++ src/pages/UserManage/UserTr.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import email from 'assets/images/user/email.png'
 import withdraw from 'assets/images/user/withdraw.png'
-import { useNavigate } from 'react-router-dom';
 
-const Tr = ({ info, onSendEmail, withdrawUser }) => {
+const Tr = ({ info, onSendEmail, withdrawUser}) => {
+
 
   const tdStyle = {
     position: 'relative',
@@ -27,8 +27,8 @@ const Tr = ({ info, onSendEmail, withdrawUser }) => {
               <td style={{ ...tdStyle, flex: '2' }}>{item.nickname}</td>
               <td style={{ ...tdStyle, flex: '3' }}>{item.email}</td>
               <td style={{ ...tdStyle, flex: '1' }}>{item.reportCount}</td>
-              <td style={tdStyle} onClick={() => onSendEmail(item)}><img src={email} style={imgStyle} /></td>
-              <td style={tdStyle} onClick={() => withdrawUser(item.userIdx)}><img src={withdraw} style={imgStyle} /></td>
+              <td style={tdStyle} onClick={(e) => onSendEmail(item)}><img src={email} style={imgStyle} /></td>
+              <td style={tdStyle} onClick={(e) => withdrawUser(item.userIdx)}><img src={withdraw} style={imgStyle} /></td>
             </tr>
           ))
         ) : (
