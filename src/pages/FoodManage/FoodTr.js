d9a93ebb98f8a6a863ec513ae67cbfe995fee147
chaerlo127 <monynony0203@gmail.com>
2023-06-03 02:04:51 +0900

    #21 feat: 레시피 식품 카테고리  수정

--- src/pages/FoodManage/FoodTr.js
+++ src/pages/FoodManage/FoodTr.js
@@ -3,21 +3,61 @@ import foodEdit from "assets/images/food/edit.png";
 import './FoodManage.css'
 import axios from 'axios';
 
-const Tr = ({ info, checkHandler, checkedStatusList, handleEdit }) => {
+const Tr = ({ info, checkHandler, checkedStatusList, setEdit }) => {
   const rows = [];
   const maxRows = 16;
 
   const [editingIndex, setEditingIndex] = useState(null);
   const [editedFoodName, setEditedFoodName] = useState('');
+  const [editFoodName, setEditFoodName] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [editCategory, setEditCategory] = useState(false);
+
+  const categoryOptions = [
+    { value: "육류", label: "육류" },
+    { value: "과일", label: "과일" },
+    { value: "채소", label: "채소" },
+    { value: "음료", label: "음료" },
+    { value: "수산물", label: "수산물" },
+    { value: "반찬", label: "반찬" },
+    { value: "조미료", label: "조미료" },
+    { value: "가공식품", label: "가공식품" },
+    { value: "기타", label: "기타" },
+  ];
+
+  const handleCategoryTdClick = (index, foodName, category) => {
+    setSelectedCategory(category);
+    setEditingIndex(index);
+    setEditedFoodName(foodName);
+    setEditFoodName(false);
+    setEditCategory(true);
+  };
+
+  const handleFoodNameTdClick = (index, foodName, category) => {
+    setSelectedCategory(category);
+    setEditingIndex(index);
+    setEditedFoodName(foodName);
+    setEditFoodName(true);
+    setEditCategory(false);
+  };
+
+  const foodNameChange = (e) => {
+    setEditedFoodName(e.target.value);
+  };
 
   const handleEditClick = (index, foodName) => {
     setEditingIndex(index);
     setEditedFoodName(foodName);
   };
 
-  const handleSave = (item) => {
+  const handleCategorySelect = (category, item) => {
+    setSelectedCategory(category);
+    handleSave(item, category);
+  };
+
+  const handleSave = (item, category) => {
     axios.patch(`/admin/foods/${item.foodIdx}`, {
-      foodCategory: item.foodCategory,
+      foodCategory: category,
       foodName: editedFoodName,
       foodImgKey: item.foodImgKey
     }).then((res) => {
@@ -26,24 +66,21 @@ const Tr = ({ info, checkHandler, checkedStatusList, handleEdit }) => {
       console.error('HTTP 요청 실패:', error);
     });
 
+
     // 저장 후 편집 상태 초기화
     setEditingIndex(null);
     setEditedFoodName('');
+    setEditFoodName(false)
+    setSelectedCategory('');
+    setEditCategory(false);
+    setEdit(true);
   };
 
-  const handleTdClick = (index, foodName) => {
-    console.log(index)
-    setEditingIndex(index);
-    setEditedFoodName(foodName);
-  };
 
-  const handleInputChange = (e) => {
-    setEditedFoodName(e.target.value);
-  };
 
   const handleKeyDown = (e, item) => {
     if (e.key === 'Enter') {
-      handleSave(item);
+      handleSave(item, item.foodCategory);
     }
   };
 
@@ -57,33 +94,61 @@ const Tr = ({ info, checkHandler, checkedStatusList, handleEdit }) => {
           <>
             <td width="5%"><input type="checkbox" checked={checkedStatusList[i]} onChange={(e) => checkHandler(item.foodIdx, i)} /></td>
             <td width="15%" >
-              <img src={item.foodImgUrl} alt="food_img"/>
+              <img src={item.foodImgUrl} alt="food_img" />
             </td>
-            <td width="15%">
-              {item.foodCategory}
-              <img className="foodEditImg" src={foodEdit} alt="food_search_img" />
+
+            <td
+              width="15%"
+              onClick={() => handleCategoryTdClick(item.foodIdx, item.foodName, item.foodCategory)}
+            >
+              {editingIndex === item.foodIdx && editCategory ? (
+                <select
+                  value={selectedCategory}
+                  onChange={(e) => {
+                    handleCategorySelect(e.target.value, item)
+                  }}
+                >
+                  <option value={selectedCategory}>{selectedCategory}</option>
+                  {categoryOptions
+                    .filter((option) => option.value !== selectedCategory)
+                    .map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                </select>
+              ) : (
+                <>
+                  {item.foodCategory}
+                  <img
+                    className="foodEditImg"
+                    src={foodEdit}
+                    alt="food_search_img"
+                  />
+                </>
+              )}
             </td>
-            <td width="15%" onClick={() => handleTdClick(item.foodIdx, item.foodName)}>
-              {editingIndex === item.foodIdx ? (
+            <td width="15%" onClick={() => handleFoodNameTdClick(item.foodIdx, item.foodName, item.foodCategory)}>
+              {editingIndex === item.foodIdx && editFoodName ? (
                 <input
                   type="text"
                   id="editInput"
                   value={editedFoodName}
-                  onChange={handleInputChange}
+                  onChange={foodNameChange}
                   onKeyDown={(e) => handleKeyDown(e, item)}
                   autoFocus
                 />
               ) : (
-              <>
-                {item.foodName}
-                <img
-                  className="foodEditImg"
-                  src={foodEdit}
-                  alt="food_search_img"
-                  onClick={() => handleEditClick(i, item.foodName)}
-                // <img className = "foodEditImg" src={foodEdit} alt="food_search_img"/>
-                />
-              </>
+                <>
+                  {item.foodName}
+                  <img
+                    className="foodEditImg"
+                    src={foodEdit}
+                    alt="food_search_img"
+                    onClick={() => handleEditClick(i, item.foodName)}
+                  // <img className = "foodEditImg" src={foodEdit} alt="food_search_img"/>
+                  />
+                </>
               )}
             </td>
           </>
@@ -101,31 +166,58 @@ const Tr = ({ info, checkHandler, checkedStatusList, handleEdit }) => {
             <td width="15%">
               <img src={item2.foodImgUrl} alt="food_img" />
             </td>
-            <td width="15%">
-              {item2.foodCategory}
-              <img className="foodEditImg" src={foodEdit} alt="food_search_img" />
+            <td
+              width="15%"
+              onClick={() => handleCategoryTdClick(item2.foodIdx, item2.foodName, item2.foodCategory)}
+            >
+              {editingIndex === item2.foodIdx && editCategory ? (
+                <select
+                  value={selectedCategory}
+                  onChange={(e) => {
+                    handleCategorySelect(e.target.value, item2)
+                  }}
+                >
+                  <option value={selectedCategory}>{selectedCategory}</option>
+                  {categoryOptions
+                    .filter((option) => option.value !== selectedCategory)
+                    .map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                </select>
+              ) : (
+                <>
+                  {item2.foodCategory}
+                  <img
+                    className="foodEditImg"
+                    src={foodEdit}
+                    alt="food_search_img"
+                  />
+                </>
+              )}
             </td>
-            <td width="15%" onClick={() => handleTdClick(item2.foodIdx, item2)}>
-              {editingIndex === item2.foodIdx ? (
+            <td width="15%" onClick={() => handleFoodNameTdClick(item2.foodIdx, item2.foodName, item2.foodCategory)}>
+              {editingIndex === item2.foodIdx && editFoodName ? (
                 <input
                   type="text"
                   id="editInput"
                   value={editedFoodName}
-                  onChange={handleInputChange}
+                  onChange={foodNameChange}
                   onKeyDown={(e) => handleKeyDown(e, item2.foodIdx)}
                   autoFocus
                 />
               ) : (
-              <>
-                {item2.foodName}
-                <img
-                  className="foodEditImg"
-                  src={foodEdit}
-                  alt="food_search_img"
-                  onClick={() => handleEditClick(i, item2.foodName)}
-                // <img className = "foodEditImg" src={foodEdit} alt="food_search_img"/>
-                />
-              </>
+                <>
+                  {item2.foodName}
+                  <img
+                    className="foodEditImg"
+                    src={foodEdit}
+                    alt="food_search_img"
+                    onClick={() => handleEditClick(i, item2.foodName)}
+                  // <img className = "foodEditImg" src={foodEdit} alt="food_search_img"/>
+                  />
+                </>
               )}
             </td>
           </>
